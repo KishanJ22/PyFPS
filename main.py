71b3cc8149315bfe6c3074f74b536f23b478dbec
KishanJ22 <kishanjethwa224@gmail.com>
2021-09-17 13:42:18 +0100

    In Version 0.2, I add a live FPS counter to the game, add keyboard key pressing and an enemy sprite that moves

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyglet  # imports the pyglet library
+from pyglet.window import key
 
 window = pyglet.window.Window()  # creates a window
 label = pyglet.text.Label('Hello World',
@@ -7,14 +8,35 @@ label = pyglet.text.Label('Hello World',
                           x=window.width // 2, y=window.height // 2,
                           anchor_x='center',
                           anchor_y='center')  # this specifies the text being shown, font name, text size and position of the text
-image = pyglet.resource.image('ps.png')  # assigns an image in the folder to the variable 'image'
+enemy_image = pyglet.image.load('tommy.png')  # assigns an image in the folder to the variable 'image'
+enemy = pyglet.sprite.Sprite(enemy_image, x=0, y=0) # takes enemy_image, makes it a sprite and gives it a starting location
+enemy.dx = 10.0 # the number of pixels the sprite will move per second
 
+def update(dt):
+    enemy.x += enemy.dx * dt
+pyglet.clock.schedule_interval(update, 1/60.0) # the sprite updates at 60Hz
+
+pyglet.clock.get_fps()
+fps_display = pyglet.window.FPSDisplay(window=window)
+
+@window.event
+def on_key_press(symbol, mofidiers):
+    if symbol == key.W:
+        print("The 'W' key was pressed.")
+    elif symbol == key.A:
+        print("The 'A' key was pressed.")
+    elif symbol == key.S:
+        print("The 'S' key was pressed.")
+    elif symbol == key.D:
+        print("The 'D' key was pressed.")
+# date: 17/09/21 - I have used Pyglet documentation to get keyboard controls working.
 
 @window.event
 def on_draw():
     window.clear()  # displays the window
     label.draw()  # displays the text in the window
-    image.blit(10, 10)  # displays the image with (x,y) sizing in the window
+    enemy.draw() # displays the enemy sprite
+    fps_display.draw() # displays the live frames per second of the game
 
 
 pyglet.app.run()  # runs the program
